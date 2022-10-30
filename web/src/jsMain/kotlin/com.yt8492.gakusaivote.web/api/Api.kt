3cdad6ae08b876623b7ef1f6c305d191491ce5e9
yt8492 <tmymyut2000@gmail.com>
2022-10-30 17:45:48 +0900

    replace base url

--- web/src/jsMain/kotlin/com.yt8492.gakusaivote.web/api/Api.kt
+++ web/src/jsMain/kotlin/com.yt8492.gakusaivote.web/api/Api.kt
@@ -20,7 +20,7 @@ import kotlinx.serialization.modules.SerializersModule
 import kotlinx.serialization.modules.contextual
 
 object Api {
-    private const val BASE_URL = "http://localhost:8080"
+    private const val BASE_URL = "https://gakusai.yt8492.com/api"
     private val client = HttpClient(Js) {
         install(ContentNegotiation) {
             json(
